985408e035bb07cc8d017041779dbd19e54082de
Rahul Vijayvargiya <33434588+1rahulvijay@users.noreply.github.com>
2024-08-02 23:40:16 +0200

    Update logger.py

--- logger.py
+++ logger.py
@@ -13,19 +13,22 @@ def app_logger(name: str) -> logging.Logger:
         file_formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
         console_formatter = logging.Formatter("%(levelname)s -- %(message)s")
 
-        # TimedRotatingFileHandler to rotate logs daily and keep only the last 10 days
-        file_handler = TimedRotatingFileHandler(log_file, when='midnight', interval=1, backupCount=10)
-        file_handler.setLevel(logging.DEBUG)
-        file_handler.setFormatter(file_formatter)
-
-        console_handler = logging.StreamHandler()
-        console_handler.setLevel(logging.INFO)  # Adjust the level to your preference
-        console_handler.setFormatter(console_formatter)
-
         logger = logging.getLogger(name)
-        logger.addHandler(file_handler)
-        logger.addHandler(console_handler)
-        logger.setLevel(logging.DEBUG)
+
+        # Check if the logger already has handlers
+        if not logger.hasHandlers():
+            # TimedRotatingFileHandler to rotate logs daily and keep only the last 10 days
+            file_handler = TimedRotatingFileHandler(log_file, when='midnight', interval=1, backupCount=10)
+            file_handler.setLevel(logging.DEBUG)
+            file_handler.setFormatter(file_formatter)
+
+            console_handler = logging.StreamHandler()
+            console_handler.setLevel(logging.INFO)  # Adjust the level to your preference
+            console_handler.setFormatter(console_formatter)
+
+            logger.addHandler(file_handler)
+            logger.addHandler(console_handler)
+            logger.setLevel(logging.DEBUG)
         
         return logger
     except OSError:
