f8dffd4e422ddad9669eeb18678fa5344062cf75
Rahul Vijayvargiya <33434588+1rahulvijay@users.noreply.github.com>
2024-08-22 19:12:49 +0200

    Update Max_id.py

--- Max_id.py
+++ Max_id.py
@@ -1,7 +1,10 @@
-SELECT
-    id,
-    MAX(your_date_column) AS max_date
-FROM
-    your_table
-GROUP BY
-    id;
+WITH LatestUpdates AS (
+    SELECT id, MAX(updated_at) AS latest_updated_at
+    FROM your_table_name
+    GROUP BY id
+)
+SELECT t.id, t.updated_at
+FROM your_table_name t
+JOIN LatestUpdates lu
+ON t.id = lu.id AND t.updated_at = lu.latest_updated_at
+WHERE lu.latest_updated_at >= DATE_SUB(CURRENT_DATE, INTERVAL 1 YEAR);
